a9f8b047eb84dcbe46fb0c7e7dbfb67f984a4e6a
damian-hirsch <damianghirsch@gmail.com>
2022-12-06 08:12:56 +0100

    Day 06

--- Day06.py
+++ Day06.py
@@ -1,32 +1,56 @@
-import numpy as np
-
-
 # Get data from .txt file
-def get_input():
+def get_input() -> str:
     with open('input/Day06.txt', 'r') as file:
-        # Split lines and write each line to list
-        data = file.read().splitlines()
-        # Map strings in list to integers
-        data = list(map(int, data))
+        # Get the string from file
+        data = file.read()
     return data
 
 
 # Solves part 1
-def part_one() -> np.ndarray:
-    test = np.zeros(5)
-    return test
+def part_one(string: str) -> int:
+    # Initialize marker
+    marker_found = False
+    # Set counter to 0
+    counter = 0
+    # While no marker is found, keep going
+    while not marker_found:
+        # Get a substring of 4 characters
+        marker = string[counter:counter + 4]
+        # Take the set and check if that set has a length of 4 (= 4 unique characters)
+        if len(set(marker)) == 4:
+            # If found, set flag to quit while loop
+            marker_found = True
+        # Else increase counter to check next substring
+        else:
+            counter += 1
+    # Add +4 to account for the number of the last character
+    return counter + 4
 
 
 # Solves part 2
-def part_two() -> int:
-
-    return 2
+def part_two(string: str) -> int:
+    # Initialize marker
+    marker_found = False
+    # Set counter to 0
+    counter = 0
+    # While no marker is found, keep going
+    while not marker_found:
+        # Get a substring of 14 characters
+        marker = string[counter:counter + 14]
+        # Take the set and check if that set has a length of 14 (= 14 unique characters)
+        if len(set(marker)) == 14:
+            # If found, set flag to quit while loop
+            marker_found = True
+        # Else increase counter to check next substring
+        else:
+            counter += 1
+    # Add +14 to account for the number of the last character
+    return counter + 14
 
 
 def main():
-
-    print('This many measurements are larger than the previous measurement: ', part_one())
-    print('This sums are larger than the previous sum: ', part_two())
+    print('This many characters need to be processed for the start-of-packet marker:', part_one(get_input()))
+    print('This many characters need to be processed for the start-of-message marker:', part_two(get_input()))
 
 
 if __name__ == '__main__':
